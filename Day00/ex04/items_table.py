b0fd8eeaec980d7aebb70c93f37263ea6b62f308
Benjamin faure <benjamin.faure@pauperas.fr>
2025-03-09 20:35:36 +0100

    Day00 ex00 to ex04

---
+++ Day00/ex04/items_table.py
@@ -0,0 +1,87 @@
+import pandas as pd
+import psycopg2
+import sqlalchemy as sqla
+import sys
+from dotenv import load_dotenv
+import os
+
+
+def load(path: str) -> pd.DataFrame:
+    """
+    Charge un fichier CSV à partir du chemin spécifié et
+    retourne un DataFrame Pandas.
+
+    Paramètres:
+    path (str): Le chemin du fichier CSV à charger.
+
+    Retourne:
+    pd.DataFrame: Un DataFrame contenant les données du fichier CSV.
+    None: Retourne None si une erreur se produit lors du chargement du fichier.
+
+    Exceptions:
+    TypeError: Si le chemin fourni n'est pas une
+    chaîne de caractères.
+    UnicodeDecodeError: Si le fichier ne peut pas être décodé
+    correctement (mauvais format).
+    FileNotFoundError: Si le fichier spécifié n'est pas trouvé.
+    """
+    try:
+        if (type(path) is not str):
+            raise TypeError("The path must be an str")
+        df = pd.DataFrame()
+        df = pd.read_csv(path)
+        print(f"Loading dataset of dimensions {df.shape}")
+        return df
+    except (UnicodeDecodeError, FileNotFoundError, TypeError) as e:
+        print(e)
+        return None
+
+def find_db_params():
+    load_dotenv("../utils/.env")
+    return {
+        'host': os.getenv('POSTGRES_HOST'),
+        'database': os.getenv('POSTGRES_DB'),
+        'user': os.getenv('POSTGRES_USER'),
+        'password': os.getenv('POSTGRES_PASSWORD')
+    }
+
+def create_items_tabls(engine):
+    meta_data = sqla.MetaData()
+
+    items = sqla.Table('items', meta_data,
+            sqla.Column('product_id', sqla.Integer, primary_key=True),
+            sqla.Column('category_id', sqla.Integer),
+            sqla.Column('category_code', sqla.String(100)),
+            sqla.Column('brand', sqla.String(50)))
+
+    meta_data.create_all(engine)
+    print("Table 'items' have been crate with succes")
+
+def import_csv_to_db(path):
+    try:
+        df = load(path)
+    except TypeError as e:
+        print(e)
+    db_params = find_db_params()
+    engine = sqla.create_engine(f"postgresql://{db_params['user']}:{db_params['password']}@{db_params['host']}/{db_params['database']}")
+
+    create_items_tabls(engine)
+
+    try:
+        db_name = f"{path.split("/")[-1].split(".csv")[0]}s"
+        print(db_name)
+        df.to_sql(db_name, engine, if_exists='replace', index=False)
+        print("Import réussi dans la base de données")
+    except Exception as e:
+        print(f"Erreur lors de l'import : {e}")
+    finally:
+        engine.dispose()
+        
+
+if __name__ == "__main__":
+    if (len(sys.argv) < 2):
+        print("ERROR : You must input at least 1 path")
+        sys.exit()
+    for i in range(1, len(sys.argv)):
+        print(sys.argv[i])
+        import_csv_to_db(sys.argv[i])
